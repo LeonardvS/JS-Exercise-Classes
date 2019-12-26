d2a8f7ce1636c714fb11925acf77cb6a93d2aef5
LeonardvS <schilcherl@gmail.com>
2019-12-26 22:03:50 +0100

    Person done, still have to implement drive for Car and rest

--- index.js
+++ index.js
@@ -77,7 +77,20 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
+  fill(gallons) {
+    return this.tank += gallons;
+  }
+
+  drive(distance) {
+
+  }
 }
 
 /*
